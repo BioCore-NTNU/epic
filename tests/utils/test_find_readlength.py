1fb55ef919985f49937e41140bc053ada979902d
Endre Bakken Stovner <endrebak85@gmail.com>
2016-05-23 15:31:13 +0200

    0.0.8: Automatically find effective genome size

--- tests/utils/test_find_readlength.py
+++ tests/utils/test_find_readlength.py
@@ -1,65 +1,25 @@
 import pytest
 
-import pandas as pd
-import numpy as np
-
 from io import StringIO
 
+from epic.utils.find_readlength import (find_readlength,
+                                        get_closest_readlength)
 
-@pytest.fixture
-def input_data():
-    pass
-
-
-@pytest.fixture
-def expected_result():
-    pass
+__author__ = "Endre Bakken Stovner https://github.com/endrebak/"
+__license__ = "MIT"
 
 
-def find_readlength(args):
+def test_get_closest_readlength():
 
-    import logging
-    from sys import platform
-    from re import search, IGNORECASE
-    from io import BytesIO
-    from subprocess import check_output
-    from epic.config import logging_settings
-    """Estimate length of reads based on 1000 first."""
-
-    bed_file = args.treatment[0]
-
-    filereader = "cat "
-    if bed_file.endswith(".gz") and search("linux", platform, IGNORECASE):
-        filereader = "zcat "
-    elif bed_file.endswith(".gz") and search("darwin", platform, IGNORECASE):
-        filereader = "gzcat "
-    elif bed_file.endswith(".bz2"):
-        filereader = "bzgrep "
-    elif bed_file.endswith(".bam"):
-        filereader = "bamToBed -i "
-
-    command = filereader + "{} | head -1000".format(bed_file)
-    output = check_output(command, shell=True)
-
-    df = pd.read_table(
-        BytesIO(output),
-        header=None,
-        usecols=[1, 2],
-        sep="\s+",
-        names=["Start", "End"])
-
-    avg_readlength = (df.End - df.Start).mean()
-
-    logging.info("Used {} to estimate an average read length of {}".format(
-        bed_file, avg_readlength))
-
-    raise "Must now implement function that finds closest length in pkg_utils"
-
-    return avg_readlength
+    assert 36 == get_closest_readlength(37)
+    assert 36 == get_closest_readlength(35)
+    assert 36 == get_closest_readlength(-1)
+    assert 50 == get_closest_readlength(50)
+    assert 50 == get_closest_readlength(62)
+    assert 100 == get_closest_readlength(1500)
 
 
 def test_find_readlength(args_200):
 
     result = find_readlength(args_200)
-    assert 0
-    # assert result == expected_result
+    assert result == 25
