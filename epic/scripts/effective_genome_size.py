60bd705eb4d1b436c7e8192588a0359399fa5c97
Ryan C. Thompson <rct@thompsonclan.org>
2016-09-30 16:57:06 -0700

    Fix temp file usage in epic-effective

--- epic/scripts/effective_genome_size.py
+++ epic/scripts/effective_genome_size.py
@@ -2,17 +2,18 @@ from __future__ import print_function, division
 
 import sys
 import atexit
-from subprocess import call, check_output
+from subprocess import check_call, check_output
 import os
 from os.path import basename
 import logging
+from tempfile import TemporaryDirectory
 
 from pyfaidx import Fasta
 
 from epic.config import logging_settings
 
 
-def effective_genome_size(fasta, read_length, nb_cores, tmpdir="/tmp"):
+def effective_genome_size(fasta, read_length, nb_cores, tmpdir=None):
     """Compute effective genome size for genome."""
 
     idx = Fasta(fasta)
@@ -35,33 +36,26 @@ def effective_genome_size(fasta, read_length, nb_cores, tmpdir="/tmp"):
             "You probably want to remove all chromosomes in your fasta containing '_' for the effective genome size computation to be accurate.",
             file=sys.stderr)
 
-    # if tmpdir is None:
-    #     try:
-    #         tmpdir = os.environ['TMPDIR']
-    #     except KeyError:
-    #         tmpdir = '/tmp'
-    output_file = os.path.join(tmpdir, '{1}.jf'.format(read_length,
-                                                       basename(fasta)))
-    atexit.register(
-        lambda: call("rm {output_file}".format(output_file=output_file), shell=True))
+    with TemporaryDirectory(prefix="epic-effective", dir=tmpdir) as output_dir:
+        output_file = os.path.join(output_dir, '{1}.jf'.format(read_length,
+                                                               basename(fasta)))
+        check_call(
+            "jellyfish count -t {nb_cores} -m {read_length} -s {genome_length} -L 1 -U 1 --out-counter-len 1 --counter-len 1 {fasta} -o {output_file}".format(
+                **vars()),
+            shell=True)
 
-    call(
-        "jellyfish count -t {nb_cores} -m {read_length} -s {genome_length} -L 1 -U 1 --out-counter-len 1 --counter-len 1 {fasta} -o {output_file}".format(
-            **vars()),
-        shell=True)
+        stats = check_output("jellyfish stats {output_file}".format(
+            output_file=output_file),
+                             shell=True)
 
-    stats = check_output("jellyfish stats {output_file}".format(
-        output_file=output_file),
-                         shell=True)
+        unique_kmers = int(stats.split()[1])
 
-    unique_kmers = int(stats.split()[1])
+        effective_genome_size = unique_kmers / genome_length
 
-    effective_genome_size = unique_kmers / genome_length
-
-    logging.info("Number unique {read_length}-mers: ".format(
-        read_length=read_length) + str(unique_kmers))
-    logging.info("Effective genome size: " + str(effective_genome_size))
-    print("Number unique {read_length}-mers: ".format(read_length=read_length),
-          unique_kmers)
-    print("Effective genome size: ", effective_genome_size)
-    assert effective_genome_size < 1, "Something wrong happened, effective genome size over 1!"
+        logging.info("Number unique {read_length}-mers: ".format(
+            read_length=read_length) + str(unique_kmers))
+        logging.info("Effective genome size: " + str(effective_genome_size))
+        print("Number unique {read_length}-mers: ".format(read_length=read_length),
+              unique_kmers)
+        print("Effective genome size: ", effective_genome_size)
+        assert effective_genome_size < 1, "Something wrong happened, effective genome size over 1!"
